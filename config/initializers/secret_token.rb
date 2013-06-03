66faa6563e7527e2708a48db238202b80a6315a2
Caleb Majo <calebmajo@yahoo.com>
2013-06-03 14:47:46 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwahiliChef::Application.config.secret_token = 'f34e654758f8cb10dd02d306313d3bb33b4b9316bd5285d45a93c92652dc4b7d0fc54155e8aaf04fa74754a123b1978e39d03f4518118ceda7391b4a60ceec7c'
